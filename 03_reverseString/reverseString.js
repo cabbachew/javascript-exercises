b90768082d2b75a51196bacad9cef673ae5c0b22
Daniel Oh <cabbachew@gmail.com>
2022-04-22 19:36:58 +0900

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let leftover = input;
+    let reversed = '';
+    for (let i = 0; i < input.length; i++) {
+        let lastLetter = leftover.slice(leftover.length - 1);
+        reversed += lastLetter;
+        leftover = leftover.slice(0, leftover.length - 1);
+    };
+    return reversed;
 };
+
+// Alternative
+// const reverseString = function(input) {
+//     return input.split("").reverse().join("");
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
