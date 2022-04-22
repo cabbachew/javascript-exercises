6004f9a08f423c3ac73b9499cfe0b47d78c3f260
Daniel Oh <cabbachew@gmail.com>
2022-04-22 23:19:42 +0900

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+// Fahrenheit to Celsius: (F - 32) x 5/9
+const ftoc = function(f) {
+  var temp = (f - 32) * 5/9;
+  return round(temp, 1);
 };
 
-const ctof = function() {
+// Celsius to Fahrenheit: C x 9/5 + 32
+const ctof = function(c) {
+  var temp = c * 9/5 + 32;
+  return round(temp, 1);
+};
 
+// Define a rounding function
+function round(num, precision = 0) {
+  var multiplier = Math.pow(10, precision);
+  return Math.round(num * multiplier) / multiplier;
 };
 
 // Do not edit below this line
