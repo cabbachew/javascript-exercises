c363f1b849d5fe8eff407483814e25f0e66f9925
Daniel Oh <cabbachew@gmail.com>
2022-04-22 18:00:31 +0900

    Solve 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(input, times) {
+    if (times < 0)
+        return 'ERROR';
+    let string = '';
+    for (let i = 0; i < times; i++) {
+        string += input;
+    };
+    return string;
 };
 
 // Do not edit below this line
