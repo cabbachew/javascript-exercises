20b9ef527a3b4056c968139ae8606618f47a967a
Daniel Oh <cabbachew@gmail.com>
2022-06-01 14:04:11 +0900

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
+const palindromes = function (string) {
+  // let cleanString = string
+  //   .toLowerCase()
+  //   .split('')
+  //   .filter(char => punctuation.indexOf(char) === -1 && char !== " ")
+  //   .join('');
+
+  // Alternate way to process string with regex
+  let cleanString = string
+    .toLowerCase()
+    .replace(/[^a-z]/g, "");
+
+  let reverseString = cleanString.split("").reverse().join("");
+  return reverseString === cleanString;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
