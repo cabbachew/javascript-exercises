452a0b610fa217da51ba8c8c27d0ab47319f4aba
Daniel Oh <cabbachew@gmail.com>
2022-06-01 14:52:04 +0900

    Solve 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let titles = [];
+  books.forEach(book => titles.push(book.title));
+  return titles;
 };
+
+// Alternate solution using map method
+// const getTheTitles = function(array) {
+//   return array.map(book => book)
+// };
 
 // Do not edit below this line
 module.exports = getTheTitles;
