3d8c2100f35370fcb4d7fc3a3a117459d1499114
Daniel Oh <cabbachew@gmail.com>
2022-06-01 14:35:28 +0900

    Make iterative solution default for 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,24 +1,23 @@
-// Using recursion
+// Solution using iteration
 const fibonacci = function(n) {
   if (n < 0) return "OOPS";
   if (n == 0) return 0;
-  if (n == 1 || n == 2) return 1;
-  return fibonacci(n-1) + fibonacci(n-2);
-};
+  let a = 0;
+  let b = 1;
+  for (i = 1; i < n; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return b;
+}
 
-// Alternate solution using iteration
+// Alternate solution using recursion
 // const fibonacci = function(n) {
 //   if (n < 0) return "OOPS";
 //   if (n == 0) return 0;
-//   let a = 0;
-//   let b = 1;
-//   for (i = 1; i < n; i++) {
-//     const temp = b;
-//     b = a + b;
-//     a = temp;
-//   }
-//   return b;
-// }
-
+//   if (n == 1 || n == 2) return 1;
+//   return fibonacci(n-1) + fibonacci(n-2);
+// };
 // Do not edit below this line
 module.exports = fibonacci;
