63635295371b58b54e81c7899dbea40bc491cbe0
Daniel Oh <cabbachew@gmail.com>
2022-06-01 12:07:13 +0900

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,65 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  array.forEach(number => {
+    total += number;
+  });
+  return total;
 };
 
-const multiply = function() {
+// Alternate solution
+// const sum = function(array) {
+//   return array.reduce((total, current) => total + current, 0);
+// };
 
+// const multiply = function(array) {
+  let product = 1;
+  array.forEach(number => {
+    product *= number;
+  })
+  return product;
 };
 
-const power = function() {
-	
+// Alternate solution
+// const multiply = function(array) {
+//   return array.length 
+//     ? array.reduce((accumulator, nextItem) => accumulator * nextItem)
+//     : 0;
+// }
+
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+// Alternate solution
+// const power = function(a,b) {
+//   return Math.power(a,b);
+// };
+
+const factorial = function(number) {
+	let result = 1;
+  for (i = 1; i <= number; i++) {
+    result *= i;
+  }
+  return result;
 };
+
+// Alternate solution
+// const factorial = function(n) {
+//   if (n === 0) return 1;
+//   let product = 1;
+//   for (let i = n; i > 0; i--) {
+//     product *= i;
+//   }
+//   return product;
+// }
 
 // Do not edit below this line
 module.exports = {
