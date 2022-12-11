46d92713df8e1a3469d351a6d6bec443e1d05248
Daniel Oh <102335309+cabbachew@users.noreply.github.com>
2022-12-11 14:43:40 +0900

    Solve 13_caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,29 @@
-const caesar = function() {
+const caesar = function(string, key) {
+  const plaintext = string.split("");
+  
+  const shift = (char) => {
+    if (char.match(/[A-Z]/)) {
+      let ord = char.charCodeAt()
+      return String.fromCharCode(
+        ((ord - 64) + (mod(key, 26))) % 26 + 64
+      );
+    } else if (char.match(/[a-z]/)) {
+      let ord = char.charCodeAt()
+      return String.fromCharCode(
+        ((ord - 96) + (mod(key,26))) % 26 + 96
+      );
+    } else {
+      return char;
+    }
+  }
 
+  // Define modulo method to return positive remainders for negative dividends
+  const mod = (n, mod) => {
+    return ((n % mod) + mod) % mod
+  }
+
+  const ciphertext = plaintext.map(shift).join("");
+  return ciphertext
 };
 
 // Do not edit below this line
